15b73809108382707db5dc854b9318192378f00b
Lovish Madaan <cs5150286@login01.hpc.iitd.ac.in>
2019-10-31 00:46:34 +0530

    Fixed module load bug in tournament.py

--- tournament.py
+++ tournament.py
@@ -1,5 +1,8 @@
 import os
 import numpy as np
+import importlib
+import sys
+import time
 
 class Tournament():
     def __init__(self, student_list, num_matches, board_size, komi):
@@ -8,9 +11,12 @@ class Tournament():
         self.board_size = board_size
         self.komi = komi
         self.folder_name = 'Tournament'
+        self.module_folder = 'modules'
 
         if not os.path.exists(self.folder_name):
             os.makedirs(self.folder_name)
+        if not os.path.exists(self.module_folder):
+            os.makedirs(self.module_folder)
     
     def run_tournament(self):
         n = len(self.student_list)
@@ -48,7 +54,7 @@ class RunMatches():
                 first_player = self.player2
                 second_player = self.player1
             match_folder = self.root_folder + '/match' + str(match_num + 1)
-            with open('tmp_match.py', 'w') as fw:
+            with open('modules/tmp_match_' + str(self.player1) + '_' + str(self.player2) + '_' + str(match_num) + '.py', 'w') as fw:
                 fw.write('import AlphaGoPlayer_' + str(first_player) + ' as Player_1\n')
                 fw.write('import AlphaGoPlayer_' + str(second_player) + ' as Player_2\n')
                 lines = None
@@ -58,11 +64,12 @@ class RunMatches():
                 for line in lines:
                     fw.write(line)
             fw.close()
-            import tmp_match as tmp_match
+            time.sleep(1)
+            tmp_match = importlib.import_module('modules.tmp_match_' + str(self.player1) + '_' + str(self.player2) + '_' + str(match_num))
             match = tmp_match.SingleMatch(self.board_size, self.komi, match_folder)
             winner, final_score = match.run_match()
 
 
-t = Tournament([1,2,3], 2, 13, 7.5)
+t = Tournament([1,2], 4, 13, 7.5)
 
 t.run_tournament()
